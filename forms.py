17a7b3c76a83fdf5401ce3e88b9ad457ae8f9c8f
Narek Martikian <Fujman94@gmail.com>
2017-09-25 23:03:14 +0300

    change signup form validators

--- forms.py
+++ forms.py
@@ -11,8 +11,8 @@ class LoginForm(FlaskForm):
 
 class SignUpForm(FlaskForm):
     email = StringField('E-mail', validators=[InputRequired(), Email(message='Invalid e-mail')])
-    username = StringField('Username', validators=[InputRequired(), Length(min=4, max=20)])
-    first_name = StringField('First name', validators=[InputRequired(), Length(min=4, max=40)])
-    last_name = StringField('Last Name', validators=[InputRequired(), Length(min=4, max=40)])
+    username = StringField('Username', validators=[InputRequired(), Length(min=3, max=20)])
+    first_name = StringField('First name', validators=[InputRequired(), Length(min=3, max=40)])
+    last_name = StringField('Last Name', validators=[InputRequired(), Length(min=3, max=40)])
     password = PasswordField('Password', validators=[InputRequired(), Length(min=6, max=80), EqualTo('confirm', message='Passwords must match')])
     confirm = PasswordField('Repeat password')
