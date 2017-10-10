2e101e9e47cf852c66a11068e3790a6a9e377fa1
Narek Martikian <Fujman94@gmail.com>
2017-10-11 00:18:31 +0300

    edited db settings

--- app.py
+++ app.py
@@ -19,7 +19,7 @@ def create_app():
     #local
     #app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql+pymysql://Fujman:1q2w3e@localhost:8889/newdb'
     # pythonwnyehre
-    app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql+pymysql://Armen:1q2w3e4r5t@Armen.mysql.pythonanywhere-services.com/Armen$newdb'
+    app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql+pymysql://Armen:1q2w3e4r5t@Armen.mysql.pythonanywhere-services.com/new$newdb'
     app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
     db.init_app(app)
     migrate = Migrate(app, db)
