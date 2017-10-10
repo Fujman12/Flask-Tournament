f62e45e6f19d84611a46156e6c446c98ab3c226c
Narek Martikian <Fujman94@gmail.com>
2017-10-11 00:40:04 +0300

    edited login form

--- forms.py
+++ forms.py
@@ -4,7 +4,7 @@ from wtforms.validators import InputRequired, Email, Length, EqualTo
 
 
 class LoginForm(FlaskForm):
-    username = StringField('Username', validators=[InputRequired(), Length(min=4, max=20)])
+    username = StringField('Username', validators=[InputRequired(), Length(min=3, max=20)])
     password = PasswordField('Password', validators=[InputRequired(), Length(min=6, max=80)])
     remember_me = BooleanField('Remember me')
 
